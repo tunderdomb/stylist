1e8d984c4b49a254cdccebdccbe0145affeda7e8
tunderdomb <tunderdomb@gmail.com>
2014-03-09 20:20:16 +0100

    data selectors, exclude commented code

--- stylist.js
+++ stylist.js
@@ -5,41 +5,71 @@
 var stylist = module.exports = {}
 
 /**
+ * Extract selectors from markup files.
  *
+ * @param content{String} the input markup
+ * @param options{Object} extract options
+ *                        default: {}
+ *
+ * @param options.ignore{String} contains already defined selectors
+ *                               default: ""
+ * @param options.style{String} the stylesheet flavor of the output (css, less, styl, stylus, sass)
+ *                              it only affects whether declaration has braces or not.
+ *                              default: "css"
+ * @param options.classes{Boolean} extract classes. default: true
+ * @param options.ids{Boolean} extract ids. default: true
+ * @param options.data{Boolean} extract data attributes. default: false
  * */
 stylist.extract = function ( content, options ){
+  options = options || {}
   var ignore = options.ignore || ""
     , style = options.style || "css"
+    , extractClasses = options.classes == undefined ? true : options.classes
+    , extractIds = options.ids == undefined ? true : options.ids
+    , extractDataAttributes = !!options.data
     , selectors = []
     // stylus doesn't have braces
     // other than that, selectors are defined the same way
     , braces = style == "styl" || style == "stylus" ? "" : "{}"
     , validSelector = /^-?[_a-zA-Z]+[_a-zA-Z0-9-]*$/
+    , match = /\s(?:class\s*=\s*"([^"]+)"|id\s*=\s*"([^"]+)"|(data-[^=\s\/>]+)(?:\s*=\s*"([^"]+)")?)/g
 
   function isDefinedElsewhere( selector ){
-    return new RegExp(selector+"\\s*" + (braces ? "{" : "(\\n|{)?")).test(ignore)
+    selector = selector.replace(/([\.\[\]])/g, "\\$1") // escape regexp entities
+    // match selectors as part of a selector chain, not just as key parts
+    return new RegExp(selector).test(ignore)
   }
 
-  content.replace(/class\s*=\s*"([^"]+)"|id\s*=\s*"([^"]+)"/g, function ( match, cls, id ){
-    if ( cls ) {
-      cls.trim().split(/\s+/).forEach(function ( cls ){
-        if( !validSelector.test(cls) ) return
-        if( isDefinedElsewhere("\\."+cls) ) return
-        cls = "." + cls + braces
-        if( !!~selectors.indexOf(cls) ) return
-        selectors.push(cls)
-      })
-    }
-    else if ( id ) {
-      if( !validSelector.test(id) ) return match
-      id = "#" + id
-      if( isDefinedElsewhere("\\"+id) ) return match
-      id += braces
-      if( !!~selectors.indexOf(id) ) return match
-      selectors.push(id)
-    }
-    return match
-  })
+  content
+    .replace(/<!--[\w\W]+?-->/g, "") // ignore anything between comments
+    .replace(match, function ( match, cls, id, dataAttr, dataVal ){
+      if ( cls && extractClasses ) {
+        cls.trim().split(/\s+/).forEach(function ( cls ){
+          if ( !validSelector.test(cls) ) return
+          if ( ignore && isDefinedElsewhere("." + cls) ) return
+          cls = "." + cls + braces
+          if ( !~selectors.indexOf(cls) ) selectors.push(cls)
+        })
+      }
+      else if ( id && extractIds ) {
+        if ( !validSelector.test(id) ) return match
+        id = "#" + id
+        if ( ignore && isDefinedElsewhere(id) ) return match
+        id += braces
+        if ( !~selectors.indexOf(id) ) selectors.push(id)
+      }
+      else if ( dataAttr && extractDataAttributes ) {
+//        if ( dataVal ) dataAttr += '="' + dataVal + '"'
+        // left here for future possibility
+        // but for now the approach is to ignore data attribute values
+        // because their values tend to be more dynamic
+        // account for attributes without values like <input data-hidden>
+        if ( ignore && isDefinedElsewhere("[" + dataAttr) ) return match
+        dataAttr = "[" + dataAttr + "]" + braces
+        if ( !~selectors.indexOf(dataAttr) ) selectors.push(dataAttr)
+      }
+      return match
+    })
 
   return selectors
 }
